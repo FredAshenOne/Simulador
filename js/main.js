c319063e8494db685011985ddece0b1e56652eeb
Ashen One <fredlam7@gmail.com>
2017-10-26 09:10:47 -0500

    new beginning

--- js/main.js
+++ js/main.js
@@ -1,24 +1,6 @@
 $(document).on('ready',function(){
 
-$('#fcfs').on('click',function(){
-  location.href = 'fcfs.html';
 
-});
-
-$('#round').on('click',function(){
-  location.href = 'round.html';
-
-});
-
-$('#prior').on('click',function(){
-  location.href = 'prior.html';
-
-});
-
-$('#sjf').on('click',function(){
-  location.href = 'sjf.html';
-
-});
 
 
 
