dba455577c949572b66459546bec56305e6bb0e7
Ashen One <fredlam7@gmail.com>
2017-10-26 10:07:19 -0500

    new beginning

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(document).on('ready',function(){
 
+  $('#add').on('click',function(){
+    if (!$('#form').hasClass('formulario')) {
+
+      $('#form').removeClass('hide');
+    $('#form').addClass('formulario');
+  }
+  });
 
 
 
